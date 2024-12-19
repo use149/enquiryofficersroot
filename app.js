3947165dc1962413018c44c5626ba31e24186ce0
use149 <welfare.ladies@saylaniwelfare.com>
2024-12-19 15:53:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,584 @@
+const officersData = {
+    "Maqsood Ahmed(149)": {
+        phone: "0315-1214355",
+        areas: [
+            "ORANGI TOWN",
+            "ORANGI SEC 10",
+            "ORANGI SEC 10L",
+            "MILLAT CLY",
+            "BANARUS",
+            "FRONTIER CLY",
+            "METROVILL",
+            "HANIFA BAD ",
+             "FAREED COLONY",
+            "NISHAN HYDER",
+            "HASHMI COLONY",
+            "RABI CLY",
+            " URDU CHOCK",
+            "KUNWARI CLY ",
+            "MUHAJIR CHOWK 10"
+        ]
+    },
+
+
+        "Ameer Abdullah (133)": {
+            phone: "03111022429",
+            areas: [
+                "MALIR15 ",
+                "MURGHI KHANA",
+                "BAKRA PEERI",
+                "RAILWAY CLY",
+                "JAM GOTH",
+                "SOMAR GOTH",
+                "DARSANO CHANNA",
+                "MEMON GOTH",
+                "FATIMA DREEM CITY",
+            ]
+        },
+    "GHULAM MUSTAFA (150)": {
+        phone: "0312-2595065",
+        areas: [
+            "ORANGI TOWN",
+            "ZIA COLONY",
+            " HARYANA CLY",
+            "BISMILLAH CLY",
+            "FAQEER CLY ",
+            " SADAF COLONY",
+            "MADINA CLY",
+            "ITTIFAQ CLY",
+            "MOMINABAD",
+            " ORANGI SEC 4",
+            "ORANGI SEC 11 ",
+            "BIJLI NAGAR",
+            "MALIK CHOwk"
+        ]
+    },
+    "TANVEER ALI (164)": {
+        phone: "03122862484",
+        areas: [
+            "ORANGI TOWN",
+            "ORANGI SEC 01 ",
+            "ORANGI SEC 8 ",
+            "ORANGI SEC  7A",
+            "ISLAMIA CLY",
+            "QASBA ",
+            "MUSLIMABAD",
+            "ORANGI sec 12",
+            "ORANGI sec 5",
+            "ORANGI SEC 13",
+            "GHAZI GOTH",
+            "ORANGI SEC 14",
+            "MUSLIM NAGAR"
+        ]
+    },
+    "TALHA(106)": {
+        phone: "03160021113",
+        areas: [
+            "ORANGI TOWN",
+            "THORANI GOTH",
+            "GULSHAN BIHAR ",
+            "ORANGI SEC 16",
+            "GULSHAN E ZIA",
+            " ORANGI11 1/2",
+            "MUHAJIR CHOWK",
+            "ALTAF NAGAR",
+            "DHABAY G",
+            "MANSOOR NAGAR"
+        ]
+    },
+    "QadeerShah(139)": {
+        phone: "0334-3979834",
+        areas: [
+            "BALDIA TOWN",
+            "SAJJAN GOTH",
+            "BALDIA 1",
+            "BALDIA 2",
+            "BALDIA 3",
+            "BALDIA 4",
+            "BALDIA 5",
+            "BALDIA 6",
+            "BALDIA 7",
+            "BALDIA 8",
+            "RASHEEDA ABAD",
+            "LEABOUR SQUIER",
+            "GULSHAN E MAZDOOR",
+        ]
+    },
+    "NAQASH (151)": {
+        phone: "0306-2087310",
+        areas: [
+
+            "MUHAJIR CAMP",
+            "GULSHAN E GHAZI",
+            "BALDIA 9",
+            "BALDIA 10",
+            "BALDIA 11",
+            "BALDIA 12",
+            "BALDIA 13",
+            "BALDIA 14",
+
+            "HUB CHOWKI",
+            "YOUSUF GOTH",
+            "MUWACH GOTH",
+            "NEVEL COLONY",
+            "NAWAB COLONY",
+            "QAIM KHANI"
+        ]
+    },
+ 
+    
+    "ABUL HUSSAIN (156)": {
+        phone: "0320-1233959",
+        areas: [
+            "KHARADAR",
+            "M.A JINNAH ROAD",
+            "KHORI GARDEN",
+            "BURNS ROAD",
+            "LIGHT HOUSE",
+            "URDU BAZAR",
+            "REGAL",
+            "SADDAR",
+            "LINES AERA",
+            "JUT LINE",
+            "BAZARTA LINE"
+        ]
+    },
+    "WAQAS SHAREEF (136)": {
+        phone: "0312-1020163",
+        areas: [
+            "NEW KARACHI ",
+            "AYUB GOTH ",
+            "NOOR MUHAMMAD GOTH",
+            "KHAMEESA GOTH",
+            "NOOR KHAN GOTH",
+            "LASSI GOTH ",
+            " DASTAGEER",
+            "YAROO GOTH  ",
+            " KHALILABAD ",
+            "KOUSAR NIAZI",
+            "YOUSUF GOTH ",
+            "SINDHI HOTEL",
+            
+        ]
+    },
+ 
+    "DANISH RAZA (92)": {
+        phone: "0319-0279867",
+        areas: [
+            "LANDHI 1",
+            "LANDHI 2",  "LANDHI 3",  "LANDHI 4","LANDHI 5",  "LANDHI 6",
+            "RERHI GOTH",
+            "DAWOOD CHOWRANGI",
+            "SHAIR PAO",
+            "KALA PANI",
+            "MUSLIMABAD",
+            "MANSEHRA CLY.",
+            "GEEDAR CLY.",
+            "89 SECTOR",
+            "GULSHAN E HADEED"
+        ]
+    },
+
+    "NOOR UDDIN(158)": {
+        phone: "0324-3268729",
+        areas: [
+            "HAKSBAY",
+            "CLIFTON",
+            " DEHLI CLY",
+            "CITY RAILWAY",
+            "BABA ISLAND",
+            " PUNJAB CLY",
+            "BHIT JAZER",
+            "KEMARI",
+            "MACCHAR CLY",
+            "MANORA",
+            "HIJRAT COLONY",
+            "MARIPUR",
+            "SHAIRSHA",
+            "MUHAMMADI COLONY",
+        
+        ]
+    },
+    "Adnan Madani (33)": {
+        phone: "03133379671",
+        areas: [
+            "NEW KARACHI",
+            "MUSLIM TOWN",
+            "UP MOR ",
+            "LAL SHAHBAZ QALANDER CLY",
+            "MUHAMMADI GOTH",
+            "NEW KARACHI SEC 5",
+            "NEW KARACHI SEC 11"
+        ]
+    },
+
+    "MEHTAB(162)": {
+        phone: "0308-3659822",
+        areas: [
+            "MADINA TOWN",
+            "GHGAR PHATAK / BIN QASIM",
+            "BIN QASIM",
+            "LABOUR SQUARE",
+            "ABDULLAH GOTH",
+            "QAIDA BAD",
+            "HASAN BAHO",
+            "GREEN CITY",
+            "JOGI MOR",
+            "MANZIL PUMP",
+            "HASHIM GOTH",
+            " RAZZAQ ABAD",
+            "CHOKANDI ",
+            "MURGHI KHANA",
+            "LASHARI GOTH",
+            "QAZAFI TOWN",
+            "PIPRI",
+            "PIPRI",
+            "ZAFAR TOWN",
+            "BHAINS CLY",
+            "MUZAFFARABAD",
+            "SAHA LATEEF",
+            "GULSHAN E HADEED"
+        ]
+    },
+    "AFTAB ALAM (31)": {
+        phone: "0311-2840049",
+        areas: [
+            "KORANGI 1 ",
+            "KORANGI 2",
+            "KORANGI 3 ",
+            "KORANGI 3 1/2 ",
+            "Y MARKET",
+            "NASIR COLONY",
+            "KORANGI  SEC 32 ",
+            "ZIA CLY ",
+            "KORANGI  SEC 33 "
+        ]},
+
+        "MUHAMMAD IMRAN(144)": {
+    "phone": "0346-7865838",
+    "areas": [
+        "MALIR",
+        "KALA BORD",
+        "MOIN ABAD",
+        "MODEL COLONY",
+        "JINAH SQUARE",
+        "SAUD ABAD",
+        "AMMAR YASIR",
+        "KHOKRAPAR 1 TO 5",
+        "SHAHFAISAL CLY",
+        "DRIG ROAD ",
+         "NATA KHAN"
+    ]
+},
+"UMAIS RAZA (163)": {
+    "phone": "03156499404",
+    "areas": [
+      
+        " CHONGI",
+        "MARI GHOT",
+        " AYYOUB GHOT",
+        "LASI GHOT ",
+        "GULSHAN E MAYMAR",
+        "SAIMA SOCIETY",
+         "AHSANABAD ",
+         "JAMALI GOTH",
+         "SCOT CLY",
+         "AFGHAN CAMP",
+         "GADAP",
+         "TOLL PLAZA ",
+         "SAFOORA GOTH",
+         " SUPER HIGHWAY ",
+         "GULZAR E HIJRI",
+          "SOHRAB GOTH ",
+          " NOORANI HOTEL "
+    ]
+}
+,
+"QARI ADNAN (147)": {
+    "phone": "0315-2988032",
+    "areas": [
+        "TARIQ ROAD",
+        "BAHADURABAD",
+        "LIAQUAT HOSPITAL",
+        " GULISTAN JOUHAR",
+        "NIPA",
+        "GULSHAN IQBAL ",
+    "DALMIA",
+        "MOSAMIAT",
+        "HASSAN SQAURE",
+        "ABUL HASAN ASFAHANI ROAD"
+    ]
+}
+,
+"NIAZ QADRI (168)": {
+    "phone": "0313-2530608",
+    "areas": [
+        "MEHMOOD ABAD",
+        "CHANESAR GOTH",
+        "AKHTAR CLY ",
+        "KALA PUL",
+        "MANZOOR CLY",
+        "DEFENCE",
+        "PECHS",
+        "KASHMIR CLY",
+        "AZAM BASTI",
+        "QAIYOOM ABAD"
+    ]
+}
+,
+"WASEEM RAZA (53)": {
+    "phone": "0316-2360800",
+    "areas": [
+        "MOOSA COLONY",
+        "KARIMABAD",
+        "MOKKA CHOK ",
+        "DASTAGEER",
+        "AEYSHA MANZIL",
+        "WATER PAMP",
+        "F.B AREA",
+         "F.B AREA",
+        "AZIZ ABAD 4..",
+        "MUJAHID COLONY",
+        "KHDOO GHOT"
+    ]
+}
+,
+"FAIZAN RAZA (110)": {
+    "phone": "0310-2206090",
+    "areas": [
+        "LIAQUTABAD 5",
+        "LIAQUTABAD 6",
+        "LIAQUTABAD 7",
+        "LIAQUTABAD 8",
+        "LIAQUTABAD 9",
+        "LIAQUTABAD 10",
+        "AZAM NAGAR ",
+        "PELI GHOTI",
+        "SINDHI HOTEL",
+        "ANGHRA GOTH",
+        "ESSA NAGRI",
+        "AZIABAD 1, 2, 3",
+          "BHONGORIA",
+            "BANHANI COLONY",
+              "F.C AREA"
+    ]
+}
+,
+"KASHIF NOORI (95)": {
+    "phone": "0317-2849944",
+    "areas": [
+        "GOLIMAR",
+        "HASRAT MOHANI COLONY",
+        "FIRDOOS COLONY",
+        " KHAMOSH COLONY",
+        "JAHAGEER ABAD",
+        "RIZWI SOCIETY",
+        "LIAQTABAD 1",
+        "LIAQTABAD 2",
+        "LIAQTABAD 3",
+        "LIAQTABAD 4",
+        "HAJI MURAD GOTH",
+        "REKSAR",
+        " PAK COLONY",
+        "ASIF COLONY ",
+        "USMANIA COLONY ",
+        "WAHEEDA BAD",
+        "JAHANGIRABAD",
+        "BARA BORD",
+        "NAZIMABAD ",
+        "ALIB COLONY",
+        " DAKHNA ",
+        "PAPOOSH",
+        " ORANGABAD",
+        "HASAN COLONY"
+    ]
+},
+
+
+"FAYYAZ AHMAD(94)": {
+    "phone": "0312-8484990",
+    "areas": [
+        "IBRAHIM HYD",
+        "COST GUARD",
+        "KORANGI SEC 48",
+        "F AREA / C AREA / R AREA",
+        " CHKRA GOTH",
+        "NORANI BASTI",
+        "KORANGI 4 ",
+         "KORANGI 5",
+          "KORANGI 6"
+    ]
+}
+,"ABDUL AZIZ (143)": {
+    "phone": "0341-2590786",
+    "areas": [
+        "JAMSHED ROAD",
+        "AL HILAL SOCIETY",
+        "TEEN HATTI",
+        "PATEL PARA",
+        " PIB COLONY",
+        "LASBELA ",
+        "JAMAT KHANA",
+        " NAFEES ABAD",
+        "  ALBELA",
+        "JHANGIR ROAD ",
+        "OLD SABZI MANDI",
+        "MARTIN QUARTER"
+    ]
+},
+"ABDUL JABBAR (148)": {
+    "phone": "0335-2840895",
+    "areas": [
+        "NORTH KARACHI ",
+        "5A1-5A4 / 5B1-5B4 / 5C1-5C4",
+        "7A-7B-7C-7D1-7D4",
+        "11 A-B-C ",
+        " AJMER NAGRI",
+        " NUSRAT BHUTTO",
+        "SHAHNAWAZ BHUTO CLY ",
+        "MIAN WALI COLONY",
+        "NOOR MUHAMMAD GOTH",
+        "KHALILABAD",
+        " BAFARZONE",
+        "KANEEZ FATIMA SOCIETY",
+        " PIYALA HOTEL",
+        "MANGO PEER",
+        "SHAH KHALID "
+    ]
+},
+
+"NIAZ QADRI(168)": {
+    "phone": "0313-2530608",
+    "areas": [
+        "MEHRAN TOWN",
+        "KORANGI IND",
+        "BILAL CHORANGI",
+        "SINGER CHORANGI",
+        "CHAMRA CHORANGI",
+        "SHAN CHORANGI",
+        "BAG E KORANGI"
+    ]
+}
+,
+"BILAL MADNI (55)": {
+    "phone": "0316-2140796",
+    "areas": [
+        "SURJANI ",
+        "SECTOR 5A-B-C-D-E-F",
+        "SECTOR 4A-B-C-D",
+        "SECTOR 7A-B-C-D",
+        "SEC 10-11",
+        "ABDULLAH MOR ",
+        "ROZI GOTH",
+        "SAIRA BB GOTH",
+        "ABBASI GOTH",
+        "GULSHAN E NOOR",
+        "SIDDIQ GOTH",
+        "KHUDA KI BASTI",
+        " LYARI EXP",
+        "52 KI PATTI ",
+        "ALI MUHAMMAD GOTH",
+        "NASRI GOTH",
+        "SURJANI SEC L-1",
+        "SAIMA VILAS",
+        "WANGI GOTH",
+        "HAMDARD UNIVERSITY"
+    ]
+}
+,
+"HASSHAM RAZA (66)": {
+    "phone": "0300-2235565",
+    "areas": [
+        "SHAH WALI ULLAH NAGAR",
+        "YAQOOB ABAD",
+        "RAEES AMROHI",
+        "ORANGI 15A-15D / SHAH FAISAL",
+        " SHAH FAISAL",
+        "ORANGE SEC 7A",
+        "ORANGE SEC 01 ",
+        " NEW L BLOCK",
+        "KHAIRABAD "
+    ]
+},
+"TARIQ MADANI (170)": {
+    "phone": "0306-3005461",
+    "areas": [
+       "CIVIL",
+       "LYARI",
+       "GARI KHATA",
+       "RANCHOR LINE",
+       "RAMSAMI",
+       "SHOE MARKET",
+       "AZEEM PLAZA",
+       "GARDEN",
+       "SOLDIER BAZAR"
+
+
+
+    ]
+}
+,
+
+}
+  
+// Populate areas dynamically in the dropdown
+window.onload = () => {
+    const areaSelect = document.getElementById("areaSelect");
+
+    Object.keys(officersData).forEach(officer => {
+        officersData[officer].areas.forEach(area => {
+            const option = document.createElement("option");
+            option.value = area;
+            option.textContent = area;
+            areaSelect.appendChild(option);
+        });
+    });
+};
+
+// Fetch officer details based on selected area
+function fetchOfficerData() {
+    const selectedArea = document.getElementById("areaSelect").value;
+    displayOfficerDetails(selectedArea);
+}
+
+// Search area dynamically
+function searchArea() {
+    const query = document.getElementById("searchArea").value.toLowerCase();
+    displayOfficerDetails(query);
+}
+
+// Display officer details based on input
+function displayOfficerDetails(area) {
+    const officerDetails = document.getElementById("officerDetails");
+    officerDetails.innerHTML = ""; // Clear previous data
+
+    if (area) {
+        let found = false;
+
+        Object.entries(officersData).forEach(([officer, details]) => {
+            const matchedAreas = details.areas.filter(a =>
+                a.toLowerCase().includes(area.toLowerCase())
+            );
+
+            if (matchedAreas.length > 0) {
+                found = true;
+                const areasHTML = details.areas
+                    .map(a =>
+                        `<div class="area ${a.toLowerCase() === area.toLowerCase() ? "highlight" : ""}">${a}</div>`
+                    )
+                    .join("");
+
+                const officerInfo = `
+                    <p><strong style="color: blue;">Officer Name:</strong> <span style="color: darkblue;">${officer}</span></p>
+                    <p><strong style="color: blue;">Contact:</strong> <span style="color: darkblue;">${details.phone}</span></p>
+                    <div class="areas-container">${areasHTML}</div>
+                `;
+                officerDetails.innerHTML = officerInfo;
+            }
+        });
+
+        if (!found) {
+            officerDetails.innerHTML = `<p style="color: red;">No results found for "${area}".</p>`;
+        }
+    }
+}
